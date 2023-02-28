4f1cedc5e1f900f934afb27c4e8ac75255254e3f
daugbjerg <daugbjerglasse@gmail.com>
2023-02-28 10:23:23 +0100

    first version

---
+++ app/src/main/java/com/example/confettiapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.confettiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import nl.dionsegijn.konfetti.core.Party;
+import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.Position;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.models.Shape;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
+
+public class MainActivity extends AppCompatActivity {
+    private KonfettiView konfettiView = null;
+    private Shape.DrawableShape drawableShape = null;
+    private Party party = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        konfettiView = findViewById(R.id.konfettiView);
+
+        EmitterConfig emitterConfig = new Emitter(200, TimeUnit.MILLISECONDS).perSecond(50);
+        party = new PartyFactory(emitterConfig)
+                .spread(360)
+                .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                .setSpeedBetween(0f, 30f)
+                .position(new Position.Relative(0.5, 0.3))
+                .build();
+    }
+}
