5be30862d6e3ec0611bb8e4ebaa1f6059d1b521c
daugbjerg <daugbjerglasse@gmail.com>
2023-02-28 10:55:48 +0100

    button that has a method called Confetti and a button with a big burst of confetti

--- app/src/main/java/com/example/confettiapp/MainActivity.java
+++ app/src/main/java/com/example/confettiapp/MainActivity.java
@@ -18,7 +18,8 @@ import nl.dionsegijn.konfetti.xml.KonfettiView;
 public class MainActivity extends AppCompatActivity {
     private KonfettiView konfettiView = null;
     private Shape.DrawableShape drawableShape = null;
-    private Party party = null;
+    private Party shortParty = null;
+    private Party longParty = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +27,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         konfettiView = findViewById(R.id.konfettiView);
 
-        EmitterConfig emitterConfig = new Emitter(200, TimeUnit.MILLISECONDS).perSecond(50);
-        party = new PartyFactory(emitterConfig)
+        EmitterConfig shortEmitterConfig = new Emitter(200, TimeUnit.MILLISECONDS).perSecond(50);
+        shortParty = new PartyFactory(shortEmitterConfig)
                 .spread(360)
                 .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
                 .setSpeedBetween(0f, 30f)
                 .position(new Position.Relative(0.5, 0.3))
                 .build();
+
+        EmitterConfig longEmitterConfig = new Emitter(500, TimeUnit.MILLISECONDS).perSecond(150);
+        longParty = new PartyFactory(longEmitterConfig)
+                .spread(500)
+                .colors(Arrays.asList(0xfce18a, 0xff726d, 0xf4306d, 0xb48def))
+                .setSpeedBetween(0f, 30f)
+                .position(new Position.Relative(0.5, 0.3))
+                .build();
+
     }
 
     public void ConfettiBoom(View view) {
-        System.out.println("Hello test");
-        konfettiView.start(party);
+        System.out.println("Confetti test");
+        konfettiView.start(shortParty);
+    }
+
+    public void bigBurstOfConfetti(View view) {
+        konfettiView.start(longParty);
     }
 }
